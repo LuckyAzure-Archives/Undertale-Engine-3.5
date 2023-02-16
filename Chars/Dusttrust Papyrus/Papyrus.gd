d8e8ca4d82746fa6cbea2ad624ca8ed88d755456
Lucky <baloghmarcellkrisztofer@gmail.com>
2023-02-16 01:21:47 +0100

    new changes

--- Chars/Dusttrust Papyrus/Papyrus.gd
+++ Chars/Dusttrust Papyrus/Papyrus.gd
@@ -5,8 +5,7 @@ var timer = 0
 func _process(delta):
 	timer += delta
 	$Legs.scale.y = 1.05 - sin(timer * 2) * 0.05
-	$Legs.scale.x = 0.95 + sin(timer * 2) * 0.05
-	$Legs/Torso.scale.y = 2 - $Legs.scale.y
-	$Legs/Torso.scale.x = 2 - $Legs.scale.x
-	$Legs/Torso.rotation_degrees = 0 + sin(timer * 2) * 1
-	$Legs/Torso/Head.rotation_degrees = -$Legs/Torso.rotation_degrees
+	$Legs.scale.x = 0.995 + sin(timer * 2 - 0.1) * 0.005
+	$Torso.position.y = -37 + sin(timer * 2 + 0.2) * 2
+	$Torso.rotation_degrees = 0 + sin(timer * 2 + 0.2) * 1
+	$Torso/Head.rotation_degrees = -$Torso.rotation_degrees
